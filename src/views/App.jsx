55dd2d7734e3e817b1169ff3203f3e7ff3f246af
Nguyễn Duy Tân-Ubuntu <nguyenduytan.08052003@gmail.com>
2025-10-27 22:51:55 +0700

    1

--- src/views/App.jsx
+++ src/views/App.jsx
@@ -1,12 +1,7 @@
-import api from "../services/callApi";
-
 function App() {
   return (
     <>
       <h1 className="text-3xl bg-red-500 font-bold underline">Hello world!</h1>
-      {api.map((item, index) => (
-        <div key={index}>{item.name}</div>
-      ))}
     </>
   );
 }
